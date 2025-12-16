cf31123b18235f2b6c3540429eb1ec1b61bf18a3
k4rtikay <katt1bh4i@gmail.com>
2025-12-16 05:02:11 +0000

    feat(useBikeStore): add state to handle bike selection

--- src/store/useBIkestore.ts
+++ src/store/useBIkestore.ts
@@ -5,11 +5,15 @@ interface BikeState {
     inseam: number;
     setHeight: (height: number) => void;
     setInseam: (inseam: number) => void;
-}
+    selectedBike: string;
+    setSelectedBike: (bike: string) => void;}
 
 export const useBikeStore = create<BikeState>((set)=>({
     height: 175,
     inseam: 80,
+    selectedBike: "duke-390-2024",
+
     setHeight: (h:number) => set({height: h}),
     setInseam: (i:number) => set({inseam: i}),
+    setSelectedBike: (id:string) => set({selectedBike: id}),
 }));
