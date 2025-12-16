df5ae2091070f5ed9e811cd50435e88aeda0aebc
k4rtikay <katt1bh4i@gmail.com>
2025-12-16 10:58:49 +0000

    feat(visualizer): hook the component to the bikeStore

--- src/app/visualizer/page.tsx
+++ src/app/visualizer/page.tsx
@@ -1,28 +1,40 @@
 "use client";
 
+import { useEffect } from "react";
 import { Controls } from "@/components/visualizer/controls";
 import { useSearchParams } from "next/navigation";
+import { useBikeStore } from "@/store/useBikeStore";
 
 export default function VisualizerPage() {
 
     const searchParams = useSearchParams();
-    const height = searchParams.get("height");
-    const inseam = searchParams.get("inseam");
+    const heightParams = searchParams.get("height");
+    const inseamParams = searchParams.get("inseam");
+    const height = useBikeStore((state) => state.height)
+    const inseam = useBikeStore((state) => state.inseam)
+    const setHeight = useBikeStore((state) => state.setHeight)
+    const setInseam = useBikeStore((state) => state.setInseam)
 
-    const parseParams = (val: string | null, fallback: number) =>{
+
+    const parseParams = (val: string | null, fallback: number) => {
         const parsed = parseInt(val || "");
         return isNaN(parsed) ? fallback : parsed;
     }
 
-    const heightValue = parseParams(height, 175);
-    const inseamValue = parseParams(inseam, 80);
+    const heightValue = parseParams(heightParams, 175);
+    const inseamValue = parseParams(inseamParams, 80);
+
+    useEffect(() => {
+        setHeight(heightValue);
+        setInseam(inseamValue);
+    }, [heightValue, inseamValue, setHeight, setInseam]);
 
     return (
         <div className="flex gap-4 h-screen">
             <aside>
                 <Controls
-                height={heightValue} inseam={inseamValue}
-                className="bg-neutral-300 h-full p-4"
+                    height={height} inseam={inseam}
+                    className="bg-neutral-300 h-full p-4"
                 />
             </aside>
             <h1 className="text-xl font-semibold">Visualizer Page</h1>
