72498fd29977e12dd25dfe1dc98350a87c820a63
k4rtikay <katt1bh4i@gmail.com>
2025-12-17 12:25:05 +0000

    feat(bikes): add new bike data

---
+++ src/config/bikes.ts
@@ -0,0 +1,32 @@
+export const bikes = [
+    {
+        id: "ktm-duke-390",
+        name: "KTM Duke 390",
+        image: "",
+        category: "naked",
+        specs: {
+            seatHeight: 820,
+            wheelBase: 1357,
+        }
+    },
+    {
+        id: "ktm-390-adventure-s",
+        name: "KTM 390 Adventure S",
+        image: "",
+        category: "adventure",
+        specs: {
+            seatHeight: 830,
+            wheelBase: 1470,
+        }
+    },
+    {
+        id: "hero-xpulse-200-4v",
+        name: "Hero XPulse 200 4V",
+        image: "",
+        category: "adventure",
+        specs: {
+            seatHeight: 825,
+            wheelBase: 1410,
+        }
+    },
+]
